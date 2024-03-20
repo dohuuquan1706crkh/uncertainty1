3a23604e808b7b3341fc5a392a75a5e6dba0ecb8
dohuuquan1706crkh <148943807+dohuuquan1706crkh@users.noreply.github.com>
2024-03-20 15:57:44 +0700

    change path

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,1 +1,1 @@
-python /content/drive/MyDrive/Segmentation-Uncertainty/src/train_reconstruction.py -t configs/tmpl.yaml -e configs/local.yaml -s 2 --exp-name entrance
+python /content/Segmentation-Uncertainty/src/train_reconstruction.py -t configs/tmpl.yaml -e configs/local.yaml -s 2 --exp-name entrance
